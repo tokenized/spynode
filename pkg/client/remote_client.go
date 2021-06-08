83ed6f52ff7cbe97992a6fde6ce8f29fe36df050
Curtis Ellis <curtis@nextcash.tech>
2021-06-08 13:54:06 -0600

    Move remote client connect metric.

--- pkg/client/remote_client.go
+++ pkg/client/remote_client.go
@@ -133,7 +133,9 @@ func (c *RemoteClient) UnsubscribePushDatas(ctx context.Context, pushDatas [][]b
 
 // SubscribeTx subscribes to information for a specific transaction. Indexes are the indexes of the
 // outputs that need to be monitored for spending.
-func (c *RemoteClient) SubscribeTx(ctx context.Context, txid bitcoin.Hash32, indexes []uint32) error {
+func (c *RemoteClient) SubscribeTx(ctx context.Context, txid bitcoin.Hash32,
+	indexes []uint32) error {
+
 	m := &SubscribeTx{
 		TxID:    txid,
 		Indexes: indexes,
@@ -144,7 +146,9 @@ func (c *RemoteClient) SubscribeTx(ctx context.Context, txid bitcoin.Hash32, ind
 }
 
 // UnsubscribeTx unsubscribes to information for a specific transaction.
-func (c *RemoteClient) UnsubscribeTx(ctx context.Context, txid bitcoin.Hash32, indexes []uint32) error {
+func (c *RemoteClient) UnsubscribeTx(ctx context.Context, txid bitcoin.Hash32,
+	indexes []uint32) error {
+
 	m := &UnsubscribeTx{
 		TxID:    txid,
 		Indexes: indexes,
@@ -582,9 +586,6 @@ func (c *RemoteClient) Close(ctx context.Context) {
 }
 
 func (c *RemoteClient) connect(ctx context.Context) (bool, error) {
-	start := time.Now()
-	defer metrics.Elapsed(ctx, start, "SpyNodeClient.connect")
-
 	c.closeRequestedLock.Lock()
 	c.closeRequested = false
 	c.closeRequestedLock.Unlock()
@@ -595,6 +596,9 @@ func (c *RemoteClient) connect(ctx context.Context) (bool, error) {
 	if c.conn != nil {
 		return false, nil // already connected
 	}
+
+	start := time.Now()
+	defer metrics.Elapsed(ctx, start, "SpyNodeClient.connect")
 
 	var connectErr error
 	for i := 0; i <= c.config.MaxRetries; i++ {
